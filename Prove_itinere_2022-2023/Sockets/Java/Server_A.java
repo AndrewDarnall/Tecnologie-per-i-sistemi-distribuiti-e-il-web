9b1f701516ad544fdef780d479be790a338d4644
Andrew R. Darnall <andrew.ryan.darnall@gmail.com>
2022-11-23 23:39:01 +0100

    added java socket itinere

---
+++ Prove_itinere_2022-2023/Sockets/Java/Server_A.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ * Soluzione proposta -> Andrew R. Darnall
+ * 
+ */
+import java.io.*;
+import java.net.*;
+import java.nio.Buffer;
+
+public class Server_A {
+
+    private static final int PORT = 3333;
+
+    public static void main(String[] args) {
+
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(PORT);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Server_A started, listening on PORT: " + PORT);
+        Socket s = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        
+
+        try{
+
+            s = server.accept();
+
+            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            out = new PrintWriter(s.getOutputStream(), true);
+
+            String msg = in.readLine();
+
+            out.print(msg + "\r\n");
+
+
+        } catch(IOException e) {
+            System.err.println("Error accepting the connection!");
+            e.printStackTrace();
+        }
+
+        try {
+            out.close();
+            in.close();
+            s.close();
+            server.close();
+        } catch (IOException e) {
+            System.err.println("Error closing the socket");
+            e.printStackTrace();
+        }
+
+    }    
+
+}
